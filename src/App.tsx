0bb12f90cdddcc9be2d3c2943b39905ef114373e
yoshi623 <yoshi623.alpha@gmail.com>
2026-01-08 19:07:25 +0900

    pause/resume/stopのUIを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,77 @@
-import { Container, Stack,  Button, Typography, CssBaseline, IconButton, } from "@mui/material";
-import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { Container, Stack,  Button, Typography, CssBaseline, IconButton, ButtonPropsColorOverrides, } from "@mui/material";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import CloseIcon from '@mui/icons-material/Close';
+import PauseIcon from '@mui/icons-material/Pause';
+import StopIcon from '@mui/icons-material/Stop';
 import MinimizeIcon from '@mui/icons-material/Minimize';
 import NumberField from "./components/NumberFields";
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 import { getCurrentWindow } from '@tauri-apps/api/window';
+import { OverridableStringUnion } from "@mui/types";
+
+
+type TimerStatus = "Idle" | "Running" | "Paused";
+
+type ButtonColorType = OverridableStringUnion<'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning', ButtonPropsColorOverrides>;
 
 function App() {
   const [minute, setMinute] = useState<number>(1);
   const [leaveSec, setLeaveSec] = useState<number>(0);
-  const [isStarted, setIsStarted] = useState<boolean>(false);
+  const [timerStatus, setTimerStatus] = useState<TimerStatus>('Idle');
 
-  const onStartTimer = () => {
-    invoke("start_timer", {
+  const startTimer = async () => {
+    await invoke("start_timer", {
       minutes: minute,
       notifyMinutes: [],
-    }).then(() => {
-      setIsStarted(true);
     });
   };
+  const pauseTimer = () => {
+    invoke("pause_timer");
+  }
+  const resumeTimer = () => {
+    invoke("resume_timer");
+  }
+
+  const buttonProps = {
+    Idle: {
+      color: 'primary',
+      startIcon: <PlayArrowIcon/>,
+      label: '開始',
+      onClick: () => {
+        startTimer();
+      },
+    },
+    Running: {
+      color: 'secondary',
+      startIcon: <PauseIcon/>,
+      label: '停止',
+      onClick: () => {
+        pauseTimer();
+      },
+    },
+    Paused: {
+      color: 'primary',
+      startIcon: <PlayArrowIcon/>,
+      label: '再開',
+      onClick: () => {
+        resumeTimer();
+      },
+    },
+  };
+
   useEffect(() => {
-    const unlistenPromise = listen<number>("timer_tick", (event) => {
+    const timerTieckListener = listen<number>("timer_tick", (event) => {
       setLeaveSec(event.payload);
-      if (event.payload === 0) {
-        setIsStarted(false);
-      }
+    });
+    const timerStatusListener = listen<TimerStatus>("timer_status", (event) => {
+      setTimerStatus(event.payload);
     });
 
     return () => {
-      unlistenPromise.then((unlisten) => unlisten());
+      timerTieckListener.then((unlisten) => unlisten());
+      timerStatusListener.then((unlisten) => unlisten());
     };
   }, []);
 
@@ -52,12 +93,28 @@ function App() {
           </IconButton>
         </Stack>
       </Stack>
-      <Container maxWidth="md">
+      <Container maxWidth="md" sx={{padding: '0px'}}>
         <Stack spacing={2} padding={2}>
-          <Stack direction={'row'} spacing={1} justifyContent={'space-between'} alignItems={'center'}>
-            <Button variant="contained" startIcon={isStarted ? <ArrowRightIcon/> : <div/>} onClick={onStartTimer} disabled={isStarted}>
-              {isStarted ? 'カウントダウン中' : 'スタート'}
-            </Button>
+          <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
+            <Stack direction={'row'} spacing={1} alignItems={'center'}>
+              <Button
+                variant="contained"
+                color={buttonProps[timerStatus].color as ButtonColorType}
+                startIcon={buttonProps[timerStatus].startIcon}
+                onClick={buttonProps[timerStatus].onClick}
+              >
+                {buttonProps[timerStatus].label}
+              </Button>
+              <Button
+                disabled={timerStatus === 'Idle'}
+                variant="contained"
+                color="error"
+                startIcon={<StopIcon/>}
+                onClick={() => {invoke('stop_timer')}}
+              >
+                停止
+              </Button>
+            </Stack>
             <Typography>
               残り&nbsp;
               {String(Math.floor(leaveSec / 60)).padStart(2, '0')}&nbsp;分
@@ -66,7 +123,7 @@ function App() {
             </Typography>
           </Stack>
           <NumberField
-            disabled={isStarted}
+            disabled={timerStatus !== 'Idle'}
             label="タイマー（分）"
             value={minute}
             max={300}
